f26d43bea2f697982f60677fc4f74d3298c482f2
RutarAndriy <RutarAndriy@gmail.com>
2024-01-13 15:39:59 +0200

    Generated project structure

---
+++ src/test/java/com/rutar/ua_translator/AppTest.java
@@ -0,0 +1,19 @@
+package com.rutar.ua_translator;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+// ............................................................................
+
+public class AppTest {
+
+///////////////////////////////////////////////////////////////////////////////
+
+@Test
+public void should_Answer_With_True()
+    { assertTrue(true); }
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
