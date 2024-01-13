4f28e4af93c038125dc5115353b634a869497c28
RutarAndriy <RutarAndriy@gmail.com>
2024-01-13 19:22:07 +0200

    Release v1.0.0

---
+++ src/main/java/com/rutar/ua_translator/UA_Translator.java
@@ -0,0 +1,64 @@
+package com.rutar.ua_translator;
+
+import javax.swing.UIManager;
+
+// ............................................................................
+// @author RutarAndriy
+// 13.01.2024
+
+public class UA_Translator {
+
+// ............................................................................
+
+private static final Object[][] ua_locale = {
+    
+    { "FileChooser.openButtonText",          "Вибрати"       },
+    { "FileChooser.openButtonToolTipText",   "Вибрати файл"  },
+    { "FileChooser.cancelButtonText",        "Відміна"       },
+    { "FileChooser.cancelButtonToolTipText", "Закрити вікно" },
+
+    { "FileChooser.openDialogTitleText",  "Вибір файлу" },
+    { "FileChooser.lookInLabelText",      "Шукати в:"   },
+    { "FileChooser.fileNameLabelText",    "Ім'я файлу:" },
+    { "FileChooser.filesOfTypeLabelText", "Тип файлів:" },
+    
+    { "FileChooser.upFolderAccessibleName", "Перейти на рівень вверх" },
+    { "FileChooser.upFolderToolTipText",    "Перейти на рівень вверх" },  
+
+    { "FileChooser.homeFolderToolTipText",    "Домашня директорія" },
+    { "FileChooser.homeFolderAccessibleName", "Домашня директорія" },
+    
+    { "FileChooser.listViewButtonToolTipText",    "Список" },
+    { "FileChooser.listViewButtonAccessibleName", "Список" },
+    
+    { "FileChooser.detailsViewButtonToolTipText",    "Детальний список" },
+    { "FileChooser.detailsViewButtonAccessibleName", "Детальний список" },
+    
+    { "FileChooser.fileNameHeaderText", "Ім'я"     },
+    { "FileChooser.fileSizeHeaderText", "Розмір"   },
+    { "FileChooser.fileTypeHeaderText", "Тип"      },
+    { "FileChooser.fileDateHeaderText", "Дата"     },
+    { "FileChooser.fileAttrHeaderText", "Атрибути" },
+    
+    { "FileChooser.readOnly", Boolean.TRUE },
+
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Ініціалізація мовного пакету
+
+public static void init() {
+
+    for (Object[] parameter : ua_locale) {
+        UIManager.put(parameter[0], parameter[1]);
+    }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+public static Object[][] get_UA_locale() { return ua_locale; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
---
+++ src/main/java/com/rutar/ua_translator/UA_Translator_Demo.java
@@ -0,0 +1,73 @@
+package com.rutar.ua_translator;
+
+// ............................................................................
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class UA_Translator_Demo {
+
+// ............................................................................
+
+private static final int INSET = 5;
+
+///////////////////////////////////////////////////////////////////////////////
+
+public static void main (String[] args) {
+
+JDialog dialog = new JDialog();
+
+dialog.setSize(300, 200);
+dialog.setResizable(false);
+dialog.setTitle("UA Translator Demo");
+
+dialog.setLayout(new GridLayout(2, 1, INSET, INSET));
+JPanel pane = (JPanel) dialog.getContentPane();
+pane.setBorder(BorderFactory.createEmptyBorder(INSET, INSET, INSET, INSET));
+
+JButton locale_default = new JButton("Стандартна локалізація");
+JButton locale_ua      = new JButton("Українська локалізація");
+
+locale_default.setActionCommand("default");
+locale_ua     .setActionCommand("ua");
+
+locale_default.addActionListener(listener);
+locale_ua     .addActionListener(listener);
+
+dialog.add(locale_default);
+dialog.add(locale_ua);
+
+dialog.setLocationRelativeTo(null);
+dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+dialog.setVisible(true);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+private static final ActionListener listener = (ActionEvent ae) -> {
+
+    String comand = ae.getActionCommand();
+
+    if (comand.equals("ua")) { UA_Translator.init(); }
+    else                     { reset_Default_Kays(); }
+
+    JFileChooser chooser = new JFileChooser();
+    chooser.showDialog(null, null);
+
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+private static void reset_Default_Kays() {
+
+    for (Object[] parameter : UA_Translator.get_UA_locale()) {
+        UIManager.put(parameter[0], null);
+        
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
